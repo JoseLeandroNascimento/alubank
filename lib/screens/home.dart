590dd462cf9d5691f0ed444f6e9d53fde936d96f
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-05-29 05:44:44 -0500

    feat: criando componente comuns

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:alubank/components/box_card.dart';
+import 'package:alubank/components/content_division.dart';
 import 'package:alubank/components/header.dart';
 import 'package:flutter/material.dart';
 
@@ -7,12 +8,12 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
           Header(),
           BoxCard(
-            boxContent: Text("Ola mundo"),
+            boxContent: ContentDivision(),
           ),
         ],
       ),
